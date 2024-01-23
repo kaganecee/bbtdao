c698f61e26986af78f34d99ccf9cc76f88638a24
kaganecee <kaganecee@gmail.com>
2024-01-24 01:11:09 +0300

    chore(contracts): deploy script corrected

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // const lockedAmount = hre.ethers.parseEther("0.001");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // // Deploy DAO contract without constructor arguments
+  // const daoContract = await hre.ethers.deployContract("DAO", {
+  //   value: lockedAmount,
+  // });
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // await daoContract.waitForDeployment();
 
-  await lock.waitForDeployment();
+  const DAO = await hre.ethers.getContractFactory("DAO");
+  const dao = await DAO.deploy()
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(dao)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
