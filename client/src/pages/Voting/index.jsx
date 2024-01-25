6cf9d91be42cd6e5098fab208067671bccfbcfc6
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-01-25 20:27:39 +0300

    voting page ui changed

--- client/src/pages/Voting/index.jsx
+++ client/src/pages/Voting/index.jsx
@@ -1,27 +1,44 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react'
 import Web3 from 'web3'
-import DAOContract from '../../DAO.json'
+import daoContract from '../../DAO.json'
 import { CONTRACT_ADDRESS } from '../../helpers/constants'
 import { getAccount } from '../../helpers/utils'
 
 const Voting = () => {
+    const [web3, setWeb3] = useState(null)
     let [proposals, setProposals] = useState([])
-    const web3 = new Web3(window.ethereum)
-    const contractABI = DAOContract.abi
-    const contract = new web3.eth.Contract(
-              contractABI,
-              CONTRACT_ADDRESS
-          )
-
-
+    const [contract, setContract] = useState([])
     useEffect(() => {
-      const updateProposals = async () => {
-        const proposalList = await fetchProposals(contract)
-        setProposals(proposalList)
+        const initWeb3 = async () => {
+            if (window.ethereum) {
+                try {
+                    const web3Instance = new Web3(window.ethereum)
+                    setWeb3(web3Instance)
+                    const contractABI = daoContract.abi
+                    const contractInstance = new web3Instance.eth.Contract(
+                        contractABI,
+                        CONTRACT_ADDRESS
+                    )
+                    setContract(contractInstance)
+                } catch (error) {
+                    console.error('Error initializing web3', error)
+                }
+            } else {
+                console.error(
+                    'Please install MetaMask or another Ethereum wallet extension'
+                )
+            }
+        }
+        initWeb3()
+
+        const updateProposals = async () => {
+            const proposalList = await fetchProposals(contract)
+            setProposals(proposalList)
         }
         updateProposals()
     }, [])
+
     const handleVoteClick = (id) => {
         setProposals((prevProposals) =>
             prevProposals.map((proposal) =>
@@ -32,21 +49,20 @@ const Voting = () => {
         )
     }
 
-    const fetchProposals = async (daoContract) => {
-        const proposalCount = await daoContract.methods.proposalCount().call()
+    const fetchProposals = async (contract) => {
+        const proposalCount = await contract.methods?.proposalCount().call()
         const _proposals = []
 
         for (let i = 0; i < proposalCount; i++) {
-            const proposal = await daoContract.methods.proposals(i).call()
+            const proposal = await contract.methods?.proposals(i).call()
             _proposals.push(proposal)
         }
         return _proposals
     }
     const voteProposal = async (proposalId) => {
         const account = await getAccount(web3)
-        console.log(account)
         try {
-            await contract.methods.vote(proposalId).send({ from: account })
+            await contract.methods?.vote(proposalId).send({ from: account })
         } catch (error) {
             console.error('Error voting for proposal', error)
         }
@@ -71,7 +87,11 @@ const Voting = () => {
                         </li>
                         {proposal.isVisible && (
                             <li>
-                                <button onClick={() => voteProposal(Number(proposal.creator))}>
+                                <button
+                                    onClick={() =>
+                                        voteProposal(Number(proposal.creator))
+                                    }
+                                >
                                     Yes
                                 </button>
                                 <button onClick={() => voteProposal('No')}>
