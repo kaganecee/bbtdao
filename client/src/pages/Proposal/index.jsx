5f7c8b3751f2f4d436b2ae701880f8b4b01ddefd
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-01-25 20:27:20 +0300

    proposal ui changed

--- client/src/pages/Proposal/index.jsx
+++ client/src/pages/Proposal/index.jsx
@@ -6,31 +6,32 @@ import { CONTRACT_ADDRESS } from '../../helpers/constants'
 import { getAccount } from '../../helpers/utils'
 
 const Proposal = () => {
-    const [web3, setWeb3] = useState(null);
+    const [web3, setWeb3] = useState(null)
     const [daoContract, setDAOContract] = useState(null)
 
     useEffect(() => {
-      const initWeb3 = async () => {
-      if (window.ethereum) {
-        try {
-          const web3Instance = new Web3(window.ethereum)
-          setWeb3(web3Instance)
-          const contractABI = DAOContract.abi
-          const contract = new web3.eth.Contract(
-              contractABI,
-              CONTRACT_ADDRESS
-          )
-          setDAOContract(contract)
-
-        } catch (error) {
-          console.error('Error initializing web3', error);
+        const initWeb3 = async () => {
+            if (window.ethereum) {
+                try {
+                    const web3Instance = new Web3(window.ethereum)
+                    setWeb3(web3Instance)
+                    const contractABI = DAOContract.abi
+                    const contract = new web3Instance.eth.Contract(
+                        contractABI,
+                        CONTRACT_ADDRESS
+                    )
+                    setDAOContract(contract)
+                } catch (error) {
+                    console.error('Error initializing web3', error)
+                }
+            } else {
+                console.error(
+                    'Please install MetaMask or another Ethereum wallet extension'
+                )
+            }
         }
-      } else {
-        console.error('Please install MetaMask or another Ethereum wallet extension');
-      }
-      }
-      initWeb3()
-    }, []);
+        initWeb3()
+    }, [])
 
     const createProposal = async (description) => {
         try {
