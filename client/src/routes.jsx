2ed6f8fec82c873fcfc26b5dec3fad5c4f5ddd22
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-01-25 20:28:25 +0300

    home page & routes updated

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,31 +1,40 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Voting from "./pages/Voting";
-import Proposal from "./pages/Proposal";
-import PageWrapper from "./pages/PageWrapper";
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import Voting from './pages/Voting'
+import Proposal from './pages/Proposal'
+import Layout from './components/Layout'
+import PageWrapper from './pages/PageWrapper'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/proposal",
-    element: (
-      <PageWrapper>
-        <Proposal />
-      </PageWrapper>
-    ),
-  },
-  {
-    path: "/voting",
-    element: (
-      <PageWrapper>
-        <Voting />
-      </PageWrapper>
-    ),
-  },
-]);
+    {
+        path: '/',
+        element: (
+            <Layout>
+                <Home />
+            </Layout>
+        ),
+    },
+    {
+        path: '/proposal',
+        element: (
+            <Layout>
+                <PageWrapper>
+                    <Proposal />
+                </PageWrapper>
+            </Layout>
+        ),
+    },
+    {
+        path: '/voting',
+        element: (
+            <Layout>
+                <PageWrapper>
+                    <Voting />
+                </PageWrapper>
+            </Layout>
+        ),
+    },
+])
 
-export default router;
+export default router
