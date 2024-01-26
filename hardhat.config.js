b7deea11678b0fba237a22741b2f0c2db8564799
kaganece <kaganece@jotform.com>
2024-01-26 11:41:42 +0300

    fix(contracts): mine in every 2 second & use multi account

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,15 +10,9 @@ module.exports = {
     hardhat: {
       mining: {
         auto: true, // Auto mine transactions
-        interval: 500000, // Mine every 5 seconds (adjust as needed)
+        interval: 20000, // Mine every 5 seconds (adjust as needed)
         chainId: 31337
-      },
-      accounts: [
-      {
-        privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-        balance: '10000000000000000000000'
       }
-    ]
     }
   },
 };
